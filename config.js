729c0f0b034d4c185d7b9708a0af11d2cfe572c8
Bony xmd <Ondiechechrispinus0@gmail.com>
2026-09-05 17:28:52 +0300

    Update and rename config.env to config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  BOT_NAME: "BONY-XMD GO V2",
+  OWNER_NAME: "Bonyke Tech",
+  OWNER_NUMBER: "2547XXXXXXXX", // put your number
+  PREFIX: ".",
+  MODE: "public", // public or private
+  VERSION: "3.0.0",
+  FOOTER: "Powered by BONY-XMD GO V2"
+}
